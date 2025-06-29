56ef1ed5f35560a7120846fe4ede316a73015fa9
arcthaneaidelta <arcthaneai@gmail.com>
2025-06-29 18:25:06 +0500

    Update zillow_api.py adding headers with cookies

--- zillow_api.py
+++ zillow_api.py
@@ -136,7 +136,9 @@ class ZillowScraper:
             "Sec-Fetch-Dest": "document",
             "Sec-Fetch-Mode": "navigate",
             "Sec-Fetch-Site": "none",
-            "Cache-Control": "max-age=0"
+            "Cache-Control": "max-age=0",
+            "Cookie": "zguid=24|%24fef1a295-9778-4650-adc4-24e2f3d36c7a; zjs_user_id=null; zjs_anonymous_id=%22fef1a295-9778-4650-adc4-24e2f3d36c7a%22"
+
         }
         
         self.min_delay = 1
